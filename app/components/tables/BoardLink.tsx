ce0c471f40d47de8d40b6d0ad903e60128b90fb4
jojoe77777 <jojoe77777@users.noreply.github.com>
2025-06-29 20:32:36 +1000

    no super prefetch :(

--- app/components/tables/BoardLink.tsx
+++ app/components/tables/BoardLink.tsx
@@ -9,7 +9,7 @@ import { getNameColor } from "@/app/utils";
 export default function BoardLink({name, uuid, url, linkUrl}: {name: string, uuid: string, url: string, linkUrl: string}) {
     const router = useRouter()
     const users = useContext(UsersContext);
-    return <Link className="boardLink" href={linkUrl} /*onMouseEnter={() => router.prefetch(`/player/${name}`)}*/ prefetch={true}>
+    return <Link className="boardLink" href={linkUrl} onMouseEnter={() => router.prefetch(`/player/${name}`)}>
         <p className="playerName" style={{color: getNameColor(users, uuid)}}>
             <img className="avatar" src={url} alt={name}/>
             {name}
